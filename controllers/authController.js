48ac038b7322d23388ce8ffaa0a263032bb61db4
Dragos Bahrim <dragosbahrim@outlook.com>
2022-05-14 15:04:50 +0300

    Fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -100,7 +100,7 @@ exports.protect = async (req, res, next) => {
 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1];
-    } else if (req.cookies.jwt) {
+    } else if (req && req.cookies && req.cookies.jwt) {
         token = req.cookies.jwt;
     }
 
